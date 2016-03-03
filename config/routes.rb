a8193253348c4e49fc1492d94e18b016f993488f
chandrasekharkolla <chandrasekharkolla@gmail.com>
2016-03-02 19:37:27 -0500

    Add raty.js to convert ratings into stars

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :reviews
   devise_for :users
   root 'movies#index'
-  resources :movies
+  resources :movies do
+      resources :reviews, except: [:index, :show]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
